647c4e554bf4f8d76cd566e215748e726abd2011
William Elliott <wjtelliott@gmail.com>
2022-08-11 15:27:00 -0500

    Uninstalled AOS, using CDN instead. Added style presets and MUI to mock landing.

--- client/src/Components/Home/LandingPage.jsx
+++ client/src/Components/Home/LandingPage.jsx
@@ -1,7 +1,8 @@
 /** @format */
 
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+import { centeredFlexBox } from '../Shared/StylePresets';
 
 const LandingPage = function () {
     const [responseData, setResponseData] = useState({ status: 'Loading' });
@@ -9,12 +10,20 @@ const LandingPage = function () {
     useEffect(() => {
         const abortContoller = new AbortController();
 
+        // This eval is required on all pages that want to use AOS
+        /* eslint-disable-next-line */
+        eval('AOS.init()');
+
         const fetchData = async () => {
             // If we are running on localhost, we have to specify port in env
             const url = `${process.env.REACT_APP_APIURL}/sanity`;
             const response = await fetch(url);
-            const resData = await response.json();
-            setResponseData(resData);
+            response
+                .json()
+                .then(data => setResponseData(data))
+                .catch(_ =>
+                    setResponseData({ error: 'Response was not JSON' })
+                );
         };
         fetchData();
 
@@ -22,17 +31,20 @@ const LandingPage = function () {
     }, []);
 
     return (
-        <div>
-            <p>Here is the env var test: {process.env.REACT_APP_TEST}</p>
-            <p>
+        <Box sx={{ ...centeredFlexBox }}>
+            <Typography variant="h3">
+                Here is the env var test:{' '}
+                {process.env.REACT_APP_TEST ?? 'NO .ENV LOADED'}
+            </Typography>
+            <Typography variant="h4">
                 Here is a request to our linked back-end:{' '}
                 {JSON.stringify(responseData)}
-            </p>
+            </Typography>
             <img
                 alt="A sports banner"
                 src="/banner.jpg"
             />
-        </div>
+        </Box>
     );
 };
 
---
+++ client/src/Components/Shared/StylePresets.js
@@ -0,0 +1,9 @@
+const stylePresets = {};
+
+stylePresets['centeredFlexBox'] = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+};
+
+module.exports = { ...stylePresets };
